182aee2f9d37442389512217aad339d1c7d649cf
John Beitler <john@coffeemedia.nl>
2016-07-18 22:58:43 +0200

    #2 - Unit tests for /Key #1 - Added @group Validate annotation

---
+++ tests/core/KeyTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class KeyTest extends TestCase {
+  public function setUp() {
+    $this->konsolidate = $GLOBALS['konsolidate'];
+  }
+
+  /**
+   * @covers /Key/create
+   * @group Core
+   * @group Key
+   * @author john@konsolidate.nl
+   */
+  public function testCreate() {
+    // Create random format
+    $format = str_shuffle('XXXXXXXXXXXXXXXXXXXX---');
+    $this->konsolidate->set('/Key/format', $format);
+    $key = $this->konsolidate->call('/Key/create');
+
+    // Positive tests
+    $this->assertEquals($this->konsolidate->get('/Key/_format'), $format);
+    $this->assertRegExp(sprintf('/%s/', str_replace('X', '\w{1}', $format)), $key);
+
+    // Negative tests
+    $format = substr($format, 1);
+    $this->assertNotEquals($this->konsolidate->get('/Key/_format'), $format);
+  }
+}
